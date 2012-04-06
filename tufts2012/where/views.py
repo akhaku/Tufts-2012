b377da5e25e2dc872a57113826fbd03c51d41702
Ammar Khaku <ammar.khaku@gmail.com>
2012-04-06 11:37:14 -0400

    Database interaction for map

--- tufts2012/where/views.py
+++ tufts2012/where/views.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 import json
@@ -25,17 +26,32 @@ def location_form(request):
             context_instance=RequestContext(request))
 
 def add_location(request):
-    name = "Bob Dylan"
-    address = quote(request.REQUEST.get('name'))
-    geocode_url = "%s?address=%s&sensor=false" % (settings.GEOCODING_URL, address)
+    address = request.REQUEST.get('name')
+    fname = request.REQUEST.get('first_name').title()
+    lname = request.REQUEST.get('last_name').title()
+    uname = "%s%s" % (fname.lower(), lname.lower())
+    geocode_url = "%s?address=%s&sensor=false" % (settings.GEOCODING_URL, quote(address))
     json_data = str(urlopen(geocode_url).read())
     obj = json.loads(json_data) # TODO catch IndexError, KeyError parse error etc
     lat = obj['results'][0]['geometry']['location']['lat']
     lon = obj['results'][0]['geometry']['location']['lng']
-    location = request.REQUEST.get('name')
+    try:
+        user = User.objects.get(username=uname)
+    except User.DoesNotExist:
+        user = User.objects.create(first_name=fname, last_name=lname, username=uname)
+    try:
+        location = user.location.get()
+        location.lat = lat
+        location.lon = lon
+        location.user = user
+        location.name = address
+        location.save()
+    except Location.DoesNotExist:
+        location = Location.objects.create(lat=lat, lon=lon, user=user, name=address)
+    name = user.get_full_name()
     form = LocationForm()
     return render_to_response('snippets/location_form.html',
             {'form' :form, 'added': True, 'lat': lat, 'lon': lon,
-                'name' :name, 'location' :location},
+                'name' :name, 'location' :address},
             context_instance=RequestContext(request))
 
