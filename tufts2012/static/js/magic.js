dcc5df697dac753acdc2fbcc2d9569b133f42728
Ammar Khaku <ammar.khaku@gmail.com>
2012-05-04 20:36:07 -0400

    Styling, wording, autocomplete tweaks

--- tufts2012/static/js/magic.js
+++ tufts2012/static/js/magic.js
@@ -177,6 +177,8 @@ function resizeGmap() {
 
 function autocompleteInit(get_url, map) {
     $('#search-box').autocomplete({
+        autoFocus: true,
+        delay: 0,
         source: get_url,
         minLength: 3,
         focus: function(e, v) {
