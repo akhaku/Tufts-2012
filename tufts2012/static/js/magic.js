409e6a43fe86f301d1ab40eb73b28bd1c74822a8
Ammar Khaku <ammar.khaku@gmail.com>
2012-05-04 20:16:01 -0400

    Bugfixes

--- tufts2012/static/js/magic.js
+++ tufts2012/static/js/magic.js
@@ -90,12 +90,14 @@ function locationFormListeners(post_url, map) {
         $.post(post_url, $('form#location-form').serialize(),
             function(data) {
                 $('#add-location-div').html(data);
-                var lat = $('#added-lat',data).html();
-                var lon = $('#added-lon',data).html();
-                var name = $('#added-name',data).html();
-                var loc = $('#added-loc',data).html();
-                placeMarker(lat, lon, name, loc, map);
-                $("#added-div").remove();
+                if ($('#added-lat').get(0)) {
+                    var lat = $('#added-lat',data).html();
+                    var lon = $('#added-lon',data).html();
+                    var name = $('#added-name',data).html();
+                    var loc = $('#added-loc',data).html();
+                    placeMarker(lat, lon, name, loc, map);
+                    $("#added-div").remove();
+                }
                 locationFormListeners(post_url, map);
             });
     });
